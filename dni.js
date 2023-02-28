72963cfa54b66d733f49211bddd2c8bc1d30b339
bzmay0149 <mayjuanbaticono@gmail.com>
2023-02-28 14:27:58 +0100

    validar dni

--- dni.js
+++ dni.js
@@ -1,16 +1,25 @@
-let validar = document.querySelector('#validationCustom05');
+let input = document.querySelector('#validationCustom05');
 let botonValidar = document.querySelector('#boton');
 
 botonValidar.addEventListener('click', validarDni);
 
 function validarDni(){
-let dni = validar.slice(8, 9);
+    let validar = input.value
+let letraDni = validar.slice(8, 9).toUpperCase();
 let numDni = parseInt(validar.slice(0, 8));
-
+alert (letraDni, numDni)
+if (isNaN(numDni) || !isNaN(letraDni)){
+    alert ('introduce 8 numeros y letra')
+}else {
 
 let letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
 let letraCorrecta = letras[numDni % 23];
 console.log (letraCorrecta);
-if(dni ==/\d{8}[a-z A-Z]/){
- alert ('introduce numero valido')
+if (letraDni == letraCorrecta){
+    alert (`tu numero ${numDni} y la letra ${letraDni} son correctas`)
+}else{
+    alert (`tu letra no coincide con tu numero`)
+}
+   
+}
 }
